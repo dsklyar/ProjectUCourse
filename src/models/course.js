4aae1656dc6b4ad4997b77e77bebf360978c2bef
DAmezcua <damezcua2@ucmerced.edu>
2017-10-01 01:55:10 -0700

    i did shit
    whooo

--- src/models/course.js
+++ src/models/course.js
@@ -3,7 +3,7 @@ Schema = mongoose.Schema;
 var mongooseUniqueValidator =  require('mongoose-unique-validator');
 
 
-var schema = new Schema({
+var schema = new Schema({ //i is in course
     registrationNumber      : {type : String, required : true, unique : true},
     title                   : {type : String, required : true},
     dateCreated             : {type : Date, required : true},
