90e9d3e572524a4d6d21e45bcabcb00e2d38edef
Daniel <danielksklyar@gmail.com>
2017-10-02 00:00:30 -0700

    broke

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -11,7 +11,7 @@ import { routing } from './app.routing';
 
 import { NgModule } from '@angular/core';
 // Add forms two way data binding
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
 
@@ -36,6 +36,7 @@ import { SidenavComponent } from './sidenav/sidenav.component';
   imports: [
     BrowserModule,
     FormsModule,
+    ReactiveFormsModule,
     BrowserAnimationsModule,
     routing,
     MatSidenavModule,
