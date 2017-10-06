5d7093df31fa1d2796894d12d5a6dafdfc1e9f12
Daniel <danielkSklyar@gmail.com>
2017-10-06 15:22:02 -0700

    Changed models

--- src/app/models/user.model.ts
+++ src/app/models/user.model.ts
@@ -1,7 +1,9 @@
+import {Course} from './course.model';
 export class User {
     constructor(public email: string,
                 public password: string,
                 public firstName?: string,
                 public lastName?: string,
-                public schoolName?: string) {}
+                public schoolName?: string,
+                public courses?: Course[]) {}
 }
