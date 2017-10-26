1a322d343c70373d128667ff0f91e4e8f0f27dfe
James Ly <jly32@ucmerced.edu>
2017-10-26 14:01:06 -0700

    added some new files for courseoverflow, still WIP

---
+++ src/app/courseQuestion/courseQuestion.component.ts
@@ -0,0 +1,30 @@
+import { Router } from '@angular/router';
+//import { AnnouncementService } from './courseQuestionService/courseQuestion.service'; //TODO
+import { Announcement } from '../models/announcement.model';
+
+
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-announcement',
+  templateUrl: '/announcement.component.html',
+  styleUrls: ['/announcement.component.css']
+})
+
+export class AnnouncementComponent {
+  @Input() announcement: Announcement;
+
+  constructor(private announcementService: AnnouncementService,
+    private router: Router) { }
+
+  onDelete() {
+    this.announcementService.removeAnnouncement(this.announcement)
+      .subscribe(
+      //result => console.log(result)
+      );
+  }
+  onEdit() {
+    this.router.navigateByUrl('/editannouncement');
+    this.announcementService.addAnnouncementToEdit(this.announcement);
+  }
+}
---
+++ src/app/models/courseQuestion.model.ts
@@ -0,0 +1,7 @@
+export class courseQuestion {
+    constructor(public title: string,
+                public courseQuestion: string,
+                public dateCreated?: Date,
+                public dateUpdated?: Date,
+                public courseQuestionID?: string){}
+}
