73b435605333da30daf96b2277958b3c6ced45cc
jameshly <jly32@ucmerced.edu>
2017-10-26 09:18:19 -0700

    more things

--- src/app/courseMenu/courseMenu.component.ts
+++ src/app/courseMenu/courseMenu.component.ts
@@ -10,18 +10,9 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['.././auth/cardcss/card.css']
 })
 
-<<<<<<< HEAD
-export class CourseMenuComponent implements OnInit {
-  courseID: string;
-=======
 export class CourseMenuComponent implements OnInit{
   selectedTab : number  = 0;
   courseID : string;
-  constructor (private activatedRoute : ActivatedRoute,
-               private announcementService : AnnouncementService,
-               private router : Router){}
->>>>>>> cc07fd13f102d2f36bfbb39dc3f1b334853dbf60
-
   constructor(private activatedRoute: ActivatedRoute,
     private announcementService: AnnouncementService,
     private router: Router,
@@ -34,7 +25,6 @@ export class CourseMenuComponent implements OnInit{
       })
     // MUST DO IT SO ANNOUNCEMENT LIST KNEW HAT COURSE IT WAS ON
     this.announcementService.setCourseID(this.courseID);
-   });
   }
   onLinkClick($event : any){
     switch ($event.index) {
