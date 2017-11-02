acc46f9f7a2c19dd702dd2f2201f557d9a9f27c7
Daniel <danielksklyar@gmail.com>
2017-11-01 21:24:08 -0700

    fixing heroku again

--- src/app/assignment/assignment.component.ts
+++ src/app/assignment/assignment.component.ts
@@ -6,8 +6,8 @@ import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-assignment',
-  templateUrl: '/assignment.component.html',
-  styleUrls: ['/assignment.component.css']
+  templateUrl: './assignment.component.html',
+  styleUrls: ['./assignment.component.css']
 })
 
 export class AssignmentComponent {
