2f7c39f9d0cb5c6be893dc1ee5c89b1b1809d3fd
Daniel <danielkSklyar@gmail.com>
2017-11-05 01:21:01 -0700

    Hacks and Hacks

--- models/assignmentQuestion.js
+++ models/assignmentQuestion.js
@@ -6,6 +6,11 @@ var mongooseUniqueValidator =  require('mongoose-unique-validator');
 var schema = new Schema({
     title                   : {type : String, required : true},
     question                : {type : String, required : true},
+    numberOfChoices         : {type : Number, required : true},
+    numberOfTries           : {type : Number, required : true},
+    pointsLostPerTry        : {type : Number, required : true},
+    pointsAvailable         : {type : Number, required : true},
+    questionBody            : {type : String, required : true},
     dateCreated             : {type : Date, required : true},
     dateUpdated             : {type : Date, required : true},
 });
